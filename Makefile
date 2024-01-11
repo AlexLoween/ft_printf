182daa043665e57d497e9798e8d0769f5481d8aa
AlexLoween <raulalanes@gmail.com>
2024-01-11 18:53:49 +0100

    iniciando printf

---
+++ Makefile
@@ -0,0 +1,20 @@
+SRC = main.c
+
+OBJS = $(SRC:.c=.o)
+
+CC = gcc
+GFLAGS = -Wall -Wextra -Werror
+RM_RF = rm -rf
+NAME = libftprintf.a
+
+$(NAME) : $(OBJS)
+	ar rcs $(NAME) $(OBJS)
+all : $(NAME)
+
+clean :
+	$(RM_RF) $(OBJS)
+fclean : clean 
+	$(RM_RF) $(NAME) $(OBJS)
+re : fclean  all
+
+.PHONY : all clean fclean re 
---
+++ ft_printf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alexlowen <alexlowen@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 18:33:41 by alexlowen         #+#    #+#             */
+/*   Updated: 2024/01/11 18:40:15 by alexlowen        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+#endif
+
+#include <stdio.h>  //para utilizar printf
+#include <unistd.h> // para utilizar el write
+#include <stdarg.h> //para las funciones variadicas
