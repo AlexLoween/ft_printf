1f2b3aac2b27381bfbd0a493b8f34f6decd7babe
AlexLoween <raulalanes@gmail.com>
2024-01-11 19:12:15 +0100

    ya funciona sin %

--- main.c
+++ main.c
@@ -6,14 +6,36 @@
 /*   By: alexlowen <alexlowen@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 18:32:45 by alexlowen         #+#    #+#             */
-/*   Updated: 2024/01/11 18:51:29 by alexlowen        ###   ########.fr       */
+/*   Updated: 2024/01/11 19:11:31 by alexlowen        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- #include "ft_printf.h"
- 
- int main ()
- {
-    printf( "hello world");
+#include "ft_printf.h"
 
- }
+int ft_printf(char const *param, ...)
+{
+    int len;
+    va_list argument;
+    
+    va_start(argument, param);
+    len = 0;
+
+    while(*param)
+    {
+        if(*param == '%')
+        {
+            param++;
+        }
+        else
+            len += write(1,param, 1);
+        param++;     
+    }
+    va_end(argument);
+    return(len);
+}
+
+int main ()
+{
+    printf( "hello world/n");
+    ft_printf("hello worrd");
+}
